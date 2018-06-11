060c7fdd6dc390d577639965fef65b3a45da7ec4
Kevin Wolf <kevinvillaloboscr@gmail.com>
2018-06-11 15:45:18 -0600

    fix build

--- src/core/utils/sleep.js
+++ src/core/utils/sleep.js
@@ -1,16 +1,2 @@
-import invariant from 'invariant'
-
-export const getEnv2 = name => {
-  const variableName = `REACT_APP_${name}`
-  const variableValue = process.env[variableName]
-
-  invariant(
-    variableValue,
-    `[${name}] variable is not defined. Please export it as ${variableName} and try again.`
-  )
-
-  return variableValue
-}
-
 const sleep = ms => new Promise(resolve => setTimeout(resolve, ms * 1000))
 export default sleep
