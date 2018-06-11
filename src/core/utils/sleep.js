2089a6640139639857abdddc4eba3dc6aa05b972
Kevin Wolf <kevinvillaloboscr@gmail.com>
2018-06-11 15:39:26 -0600

    add duplicated code

--- src/core/utils/sleep.js
+++ src/core/utils/sleep.js
@@ -1,3 +1,16 @@
-const sleep = ms => new Promise(resolve => setTimeout(resolve, ms * 1000))
+import invariant from 'invariant'
 
+export const getEnv = name => {
+  const variableName = `REACT_APP_${name}`
+  const variableValue = process.env[variableName]
+
+  invariant(
+    variableValue,
+    `[${name}] variable is not defined. Please export it as ${variableName} and try again.`
+  )
+
+  return variableValue
+}
+
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms * 1000))
 export default sleep
