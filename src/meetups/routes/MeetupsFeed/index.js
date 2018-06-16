d93613e1b2ae53c4962b59926c7e914ddaba8969
Kevin Wolf <kevinvillaloboscr@gmail.com>
2018-06-16 16:44:01 -0600

    add redux-render

--- src/meetups/routes/MeetupsFeed/index.js
+++ src/meetups/routes/MeetupsFeed/index.js
@@ -1,34 +1,35 @@
 import React from 'react'
+import { Redux } from 'redux-render'
 import Component from '@reactions/component'
 import { select } from '@rematch/select'
 
-import { Connect } from 'core/utils'
 import { Container, Heading } from 'core/components'
 import { MeetupsList } from 'meetups/components'
 
 const MeetupsFeed = () => (
-  <Connect
-    select={state => ({
+  <Redux
+    selector={state => ({
       isLoading: state.loading.effects.meetups.fetch,
       meetups: select.meetups.all(state)
     })}
   >
-    {({ state, dispatch }) => (
+    {(state, dispatch) => (
       <Component
         didMount={() => {
           dispatch.meetups.fetch()
         }}
-      >
-        <Container>
-          <Heading border mb={4}>
-            Upcoming Meetups
-          </Heading>
+        render={() => (
+          <Container>
+            <Heading border mb={4}>
+              Upcoming Meetups
+            </Heading>
 
-          <MeetupsList isLoading={state.isLoading} meetups={state.meetups} />
-        </Container>
-      </Component>
+            <MeetupsList isLoading={state.isLoading} meetups={state.meetups} />
+          </Container>
+        )}
+      />
     )}
-  </Connect>
+  </Redux>
 )
 
 export default MeetupsFeed
